caead0e341718b097975b21bd710f3a5e4e4bdcc
a8anassis <a8anassis@gmail.com>
2026-01-16 19:22:17 +0200

    feat: add abstract entity

---
+++ src/main/java/gr/aueb/cf/edusevapp/model/AbstractEntity.java
@@ -0,0 +1,28 @@
+package gr.aueb.cf.edusevapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
